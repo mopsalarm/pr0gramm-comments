490e9e84e12ec7c12e1fdca03fdd01f295b3c240
Mopsalarm <mopsalarm@users.noreply.github.com>
2017-02-07 21:44:58 +0100

    Cleanup schema.

--- schema.sql
+++ schema.sql
@@ -16,11 +16,4 @@ CREATE TABLE IF NOT EXISTS comment_favorites (
     flags INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_token (
-  mail_hash VARCHAR(32) NOT NULL,
-  token VARCHAR(32) NOT NULL
-);
-
-
 CREATE UNIQUE INDEX IF NOT EXISTS "comment_favorites__fav_owner__id" ON comment_favorites(fav_owner, id);
-CREATE UNIQUE INDEX IF NOT EXISTS "user_token__mail_hash" ON user_token(mail_hash);
